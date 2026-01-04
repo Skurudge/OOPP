4729cedfaf4c832ed45bbe872b6136fc61404977
Konstantin Bogatyrev <k.grumant@gmail.com>
2026-01-04 13:19:06 +0300

    Добавлено тестирование в test_product.py для увеличения покрытия.

--- tests/test_product.py
+++ tests/test_product.py
@@ -50,3 +50,33 @@ def test_product_zero_quantity():
         ValueError, match="Товар с нулевым количеством не может быть добавлен"
     ):
         Product("Iphone 15", "512GB, Gray space", 210000.0, 0)
+
+
+def test_product_from_dict(product_sample_dict):
+    product = Product.new_product(product_sample_dict)
+    assert str(product) == "Samsung Galaxy S23 Ultra, 180000.0 руб., Остаток: 5 шт."
+
+
+def test_update_or_add__update():
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    product4 = Product("Iphone 15", "512GB, Gray space", 240000.0, 12)
+
+    product_list = Product.update_or_add(product4, [product2, product3])
+
+    assert len(product_list) == 2
+    assert str(product_list[0]) == "Iphone 15, 240000.0 руб., Остаток: 20 шт."
+    assert str(product_list[1]) == "Xiaomi Redmi Note 11, 31000.0 руб., Остаток: 14 шт."
+
+
+def test_update_or_add__add():
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    product4 = Product("Iphone 17", "512GB, Black space", 250000.0, 1)
+
+    product_list = Product.update_or_add(product4, [product2, product3])
+
+    assert len(product_list) == 3
+    assert str(product_list[0]) == "Iphone 15, 210000.0 руб., Остаток: 8 шт."
+    assert str(product_list[1]) == "Xiaomi Redmi Note 11, 31000.0 руб., Остаток: 14 шт."
+    assert str(product_list[2]) == "Iphone 17, 250000.0 руб., Остаток: 1 шт."
